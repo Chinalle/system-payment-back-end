3db067a363865e6383ca28b47f840697bc2fdd56
OliveiraLeonidas <leonidasoliv25@gmail.com>
2025-09-10 19:05:34 -0300

    feat: add node mailer service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './users/user.module';
 import { SeedModule } from './database/seeds/seed.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    MailerModule,
     DatabaseModule,
     UserModule,
     SeedModule,
