6276a53862f9fca9fc4e536f40f460f151e4fad3
Chinalle <octaviogomes167@gmail.com>
2025-09-29 07:37:44 -0300

    added to the provider's schedule tables

---
+++ src/entities/schedule-break.entity.ts
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    JoinColumn,
+} from 'typeorm';
+import { WorkSchedule } from './work-schedule.entity';
+
+@Entity('schedule_breaks')
+export class ScheduleBreak {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 100 })
+    name: string;
+
+    @Column({ type: 'time', name: 'start_time' })
+    startTime: string; 
+
+    @Column({ type: 'time', name: 'end_time' })
+    endTime: string; 
+
+    @ManyToOne(() => WorkSchedule, (workSchedule) => workSchedule.scheduleBreaks)
+    @JoinColumn({ name: 'work_schedule_id' })
+    workSchedule: WorkSchedule;
+}
