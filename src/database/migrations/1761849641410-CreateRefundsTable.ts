9dbe07da01033b1df71ada08ebd6d2d56c7c0c02
OliveiraLeonidas <leonidasoliv25@gmail.com>
2025-10-30 19:12:45 -0300

    feat: add refund table

---
+++ src/database/migrations/1761849641410-CreateRefundsTable.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRefundsTable1761849641410 implements MigrationInterface {
+  // [x] Nova Tabela refund: Criar a tabela de estornos com as colunas: id, payment_id (FK), stripe_refund_id (string, unique), amount_in_cents, reason, status, created_at.
+  private tableName = 'refunds';
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          { name: 'id', type: 'uuid', isPrimary: true },
+          { name: 'payment_id', type: '' },
+          { name: 'stripe_refund_id', type: 'text' },
+          { name: 'amount_in_cents', type: 'int' },
+          { name: 'reason', type: 'int' },
+          { name: 'status', type: 'text' },
+          { name: 'created_at', type: 'timestamptz', default: 'now()' },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.tableName);
+  }
+}
