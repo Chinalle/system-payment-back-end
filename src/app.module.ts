39a349f3d9a6f6e1192c03161ffcab9eb82d8928
debora-correa-menezes@hotmail.com <debora-correa-menezes@hotmail.com>
2025-11-06 12:03:45 -0300

    feat: availability with breaks and overrides

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { UserModule } from './users/user.module';
     ServiceModule,
     QuotationModule,
     QuotationRequestModule,
+    AvailabilityModule,
     PaymentsModule,
     AvailabilityModule,
     PaymentsModule,
